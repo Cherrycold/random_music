9e8b0e653ef8bcbfa25af1c0b015f569b6f0b996
iris <60179904@qq.com>
2020-08-26 21:23:33 +0800

    add config

--- src/music.py
+++ src/music.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2020-04-02 13:54:57
-@LastEditTime: 2020-08-24 10:43:00
+@LastEditTime: 2020-08-26 21:21:14
 @LastEditors: your name
 @Description: 
 @FilePath: \src\music.py
@@ -17,6 +17,20 @@ import tkinter.scrolledtext as scrolledtext
 import _thread
 import threading
 import queue
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini',encoding="UTF-8")
+config_list = config['config']
+
+_path = config_list['path'].replace('\\','/')
+_time = config_list['time']
+_Intervals_min = config_list['Intervals_min']
+_Intervals_max = config_list['Intervals_max']
+# _time = config.get('config',"time")
+# _Intervals_min = config.get('config',"Intervals_min")
+# _Intervals_max = config.get('config',"Intervals_max")
+
 
 msg_type = {
     "start" : 1,
@@ -32,26 +46,28 @@ def file_name(path):
     return list
 
 class logmgr(object):
-    def __init__(self):
+    def __init__(self,player):
         rootpath = os.path.abspath(os.path.dirname(os.getcwd()))
         folder = os.path.exists(rootpath+"/log")
         if not folder:
             os.makedirs(rootpath+"/log")
         filename = rootpath+"/log/"+time.strftime("%m_%d_%H", time.localtime())
         self.file = open(filename,'w')
+        self.player = player
     def logout(self,outstr):
         log = "[ "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+" ] : " + outstr + "\n"
         self.file.write(log)
         self.file.flush()
+        self.player.scr.insert(END,log)
+        self.player.scr.update()
         return log
 
 class Player(object):
     def __init__(self):
         self.flag = TRUE
-        self.path = "F:/test"
+        self.path = _path[1:-1]
+        self.log = logmgr(self)
         self.wks = file_name(self.path)
-        
-        self.log = logmgr()
         
         self.stop_flag = FALSE
         
@@ -59,10 +75,14 @@ class Player(object):
         mixer.init()
 
     def playMp3(self):
+        if len(self.wks) == 0:
+            self.log.logout("没有声音文件")
+            return
         self.flag = TRUE
         num = random.randint(0,len(self.wks)-1)
-        self.scr.insert(END,self.log.logout("Play "+self.wks[num]))
-        self.scr.update()
+        self.log.logout("Play "+self.wks[num])
+        # self.scr.insert(END,self.log.logout("Play "+self.wks[num]))
+        # self.scr.update()
         mixer.music.load(self.path+"/"+self.wks[num])
         mixer.music.play(1)
 
@@ -70,10 +90,12 @@ class Player(object):
         if self.flag == FALSE:
             mixer.music.unpause()
             self.flag = TRUE
-            self.scr.insert(END,self.log.logout("Unpause"))
+            self.log.logout("Unpause")
+            # self.scr.insert(END,self.log.logout("Unpause"))
             return
         mixer.music.pause()
-        self.scr.insert(END,self.log.logout("Pause"))
+        self.log.logout("Pause")
+        # self.scr.insert(END,self.log.logout("Pause"))
         self.flag = FALSE
     def open_over_thread(self):
         #启动线程监听结束
@@ -85,6 +107,9 @@ class Player(object):
                 break
         return
     def auto_start(self):
+        if len(self.wks) == 0:
+            self.log.logout("没有声音文件")
+            return
         self.over.place_forget()
         self.start.place_forget()
         self.restart.place_forget()
@@ -93,15 +118,17 @@ class Player(object):
         t1 = threading.Thread(target=self.open_over_thread)
         t1.setDaemon(True)
         t1.start()
-        threading.Timer(60*3,self.auto_stop).start()
-        self.scr.insert(END,self.log.logout("after 3min over"))
+        threading.Timer(60 * _time ,self.auto_stop).start()
+        self.log.logout("after 3min over")
+        # self.scr.insert(END,self.log.logout("after 3min over"))
         self.scr.update()
     def auto_stop(self):
         self.pause_music()
         self.stop_flag = FALSE
-        randomtime = random.randint(40,60)*60
+        randomtime = random.randint(_Intervals_min,_Intervals_max)*60
         threading.Timer(randomtime,self.auto_start).start()
-        self.scr.insert(END,self.log.logout("after "+str(randomtime/60)+"min restart"))
+        self.log.logout("after "+str(randomtime/60)+"min restart")
+        # self.scr.insert(END,self.log.logout("after "+str(randomtime/60)+"min restart"))
         self.scr.update()
     def painter(self):
         frame1 = Tk()
